c7838657168bedd85f256ed34cae13c36455c208
S29 <nots29vv@gmail.com>
2024-06-14 19:09:18 +0100

    first commit

--- app/frames/NameInput/route.ts
+++ app/frames/NameInput/route.ts
@@ -17,3 +17,10 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
       <meta property="fc:frame:post_url" content="/"/>
       </head></html>`)
 }
+
+export async function POST(req: NextRequest): Promise<Response> {
+    return getResponse(req);
+}
+  
+export const dynamic = 'force-dynamic';
+  
