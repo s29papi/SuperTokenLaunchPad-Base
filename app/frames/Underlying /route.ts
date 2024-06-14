12ca50aefe65d23ded2d5ccaff8ebf136fd253fe
S29 <nots29vv@gmail.com>
2024-06-14 20:11:36 +0100

    first commit

--- app/frames/Underlying /route.ts
+++ app/frames/Underlying /route.ts
@@ -6,16 +6,18 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
     const body: FrameRequest = await req.json();
     const searchParams = req.nextUrl.searchParams;
     const tokenName:any = searchParams.get("tokenName");
-    const ogImageUrl = new URL(`/og/Upgradable`, FRAMES_URL).href
+    const tokenSymbol:any = searchParams.get("tokenSymbol");
+    const ogImageUrl = new URL(`/og/Underlying`, FRAMES_URL).href
     const upgradeable = body.untrustedData.buttonIndex
     return new NextResponse(`<!DOCTYPE html><html><head>
       <title>Upgradability</title>
       <meta property="fc:frame" content="vNext" />
       <meta property="of:accepts:xmtp" content="2024-02-01" /> 
       <meta property="fc:frame:image" content="${ogImageUrl}"/>
+      <meta property="fc:frame:input:text" content="> Underlying Token Address"/>
       <meta property="fc:frame:button:1" content="Deploy Token" />
       <meta property="fc:frame:button:1:action" content="post"/>
-      <meta property="fc:frame:post_url" content="${FRAMES_URL}/frames/underlying?tokenName=${tokenName}&&tokenSymbol=${tokenSymbol}"/>
+      <meta property="fc:frame:post_url" content="${FRAMES_URL}/frames/underlying?tokenName=${tokenName}&&tokenSymbol=${tokenSymbol}&&upgradeable=${upgradeable}"/>
       </head></html>`)
 }
 
