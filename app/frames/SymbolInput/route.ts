59c1602c00054a631bcac331cb8480cf4b45da8b
S29 <nots29vv@gmail.com>
2024-06-14 19:31:50 +0100

    first commit

--- app/frames/SymbolInput/route.ts
+++ app/frames/SymbolInput/route.ts
@@ -3,18 +3,18 @@ import { NextRequest, NextResponse } from 'next/server';
   
 const FRAMES_URL = process.env.FRAMES_URL || "https://super-token-launch-pad-base.vercel.app"
 async function getResponse(req: NextRequest): Promise<NextResponse> { 
-    
-    const ogImageUrl = new URL(`/og/NameInput`, FRAMES_URL).href
+    const body: FrameRequest = await req.json();
+    const ogImageUrl = new URL(`/og/SymbolInput`, FRAMES_URL).href
 
     return new NextResponse(`<!DOCTYPE html><html><head>
       <title>Input Token Name</title>
       <meta property="fc:frame" content="vNext" />
       <meta property="of:accepts:xmtp" content="2024-02-01" /> 
       <meta property="fc:frame:image" content="${ogImageUrl}"/>
-      <meta property="fc:frame:input:text" content="> Token Name"/>
+      <meta property="fc:frame:input:text" content="> Token Symbol"/>
       <meta property="fc:frame:button:1" content="Next" />
       <meta property="fc:frame:button:1:action" content="post"/>
-      <meta property="fc:frame:post_url" content="${FRAMES_URL}/frames/SymbolInput"/>
+      <meta property="fc:frame:post_url" content="/"/>
       </head></html>`)
 }
 
