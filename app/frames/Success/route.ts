bf1ef25c8f7f2cb783fb5f17e5c5d00628ae325a
S29 <nots29vv@gmail.com>
2024-06-14 22:31:17 +0100

    first commit

--- app/frames/Success/route.ts
+++ app/frames/Success/route.ts
@@ -5,14 +5,14 @@ const FRAMES_URL = process.env.FRAMES_URL || "https://super-token-launch-pad-bas
 async function getResponse(req: NextRequest): Promise<NextResponse> { 
     const body: FrameRequest = await req.json();
     const ogImageUrl = new URL(`/og/Success`, FRAMES_URL).href
-    const post_Url = new URL(`/frames?txhash=${body.untrustedData.transactionId}`, FRAMES_URL).href
+    const post_Url = new URL(`/frames/pre-redirect?txhash=${body.untrustedData.transactionId}`, FRAMES_URL).href
     
     return new NextResponse(`<!DOCTYPE html><html><head>
       <title>Upgradability</title>
       <meta property="fc:frame" content="vNext" />
       <meta property="of:accepts:xmtp" content="2024-02-01" /> 
       <meta property="fc:frame:image" content="${ogImageUrl}"/>
-      <meta property="fc:frame:button:1" content="" />
+      <meta property="fc:frame:button:1" content="Share" />
       <meta property="fc:frame:button:1:action" content="post_redirect"/>
       <meta property="fc:frame:post_url" content="${post_Url}"/>
       </head></html>`)
