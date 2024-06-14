11d7ceb857f91430e35e4eacfc84cee447168118
S29 <nots29vv@gmail.com>
2024-06-14 20:47:26 +0100

    first commit

--- app/frames/Underlying /route.ts
+++ app/frames/Underlying /route.ts
@@ -3,12 +3,12 @@ import { NextRequest, NextResponse } from 'next/server';
   
 const FRAMES_URL = process.env.FRAMES_URL || "https://super-token-launch-pad-base.vercel.app"
 async function getResponse(req: NextRequest): Promise<NextResponse> { 
-    const body: FrameRequest = await req.json();
-    const searchParams = req.nextUrl.searchParams;
-    const tokenName:any = searchParams.get("tokenName");
-    const tokenSymbol:any = searchParams.get("tokenSymbol");
+    // const body: FrameRequest = await req.json();
+    // const searchParams = req.nextUrl.searchParams;
+    // const tokenName:any = searchParams.get("tokenName");
+    // const tokenSymbol:any = searchParams.get("tokenSymbol");
     const ogImageUrl = new URL(`/og/Underlying`, FRAMES_URL).href
-    const upgradeable = body.untrustedData.buttonIndex
+    // const upgradeable = body.untrustedData.buttonIndex
     return new NextResponse(`<!DOCTYPE html><html><head>
       <title>Upgradability</title>
       <meta property="fc:frame" content="vNext" />
