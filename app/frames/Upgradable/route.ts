c4363bfd86f105346adc3ee888f7b4fbb6e526c5
S29 <nots29vv@gmail.com>
2024-06-14 20:32:58 +0100

    first commit

--- app/frames/Upgradable/route.ts
+++ app/frames/Upgradable/route.ts
@@ -8,7 +8,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
     const tokenName:any = searchParams.get("tokenName");
     const ogImageUrl = new URL(`/og/Upgradable`, FRAMES_URL).href
     const tokenSymbol = body.untrustedData.inputText
-    const post_Url = new URL(`/frames/Underlying?tokenName=${tokenName}&&tokenSymbol=${tokenSymbol}`, FRAMES_URL).href
+    const post_Url = new URL(`/frames/Underlying?tokenName=${tokenName}`, FRAMES_URL).href
     return new NextResponse(`<!DOCTYPE html><html><head>
       <title>Upgradability</title>
       <meta property="fc:frame" content="vNext" />
@@ -16,7 +16,8 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
       <meta property="fc:frame:image" content="${ogImageUrl}"/>
       <meta property="fc:frame:button:1" content="Yes" />
       <meta property="fc:frame:button:1:action" content="post"/>
-    
+      <meta property="fc:frame:button:2" content="No" />
+      <meta property="fc:frame:button:2:action" content="post"/>
       <meta property="fc:frame:post_url" content="${post_Url}"/>
       </head></html>`)
 }
