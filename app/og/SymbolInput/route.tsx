a0b4da6a75aed6bceac0c60903f26128fff40756
S29 <nots29vv@gmail.com>
2024-06-14 19:33:41 +0100

    first commit

---
+++ app/og/SymbolInput/route.tsx
@@ -0,0 +1,28 @@
+import { ImageResponse } from 'next/og';
+import { NextRequest } from 'next/server';
+
+export const runtime = 'edge';
+
+
+
+export async function GET(req: NextRequest) {
+    const kanitFontData = await fetch(
+        new URL('../../public/Kanit-ExtraBold.ttf', import.meta.url),
+    ).then((res) => res.arrayBuffer());
+    return new ImageResponse(
+        (
+            <div tw='flex w-full h-full justify-center items-center text-6xl text-white bg-blue-500' style={{fontFamily: 'Kanit ExtraBold'}}>
+                2. Input Token Symbol ? 
+            </div>
+        ),
+        {
+            fonts: [
+                {
+                    name: 'Kanit ExtraBold',
+                    data: kanitFontData,
+                    style: 'normal'
+                },
+            ]
+        }
+    )
+}
