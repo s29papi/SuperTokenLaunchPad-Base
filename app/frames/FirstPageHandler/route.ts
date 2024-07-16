c12fe7554446ef9c3cb7dc5c4f39b545a9e8d790
S29 <nots29vv@gmail.com>
2024-07-16 10:04:10 +0100

    commented inputTest

--- app/frames/FirstPageHandler/route.ts
+++ app/frames/FirstPageHandler/route.ts
@@ -24,19 +24,18 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
     
     if (idx == 2) {
                        
-        const actionUrl = `https://warpcast.com/~/add-cast-action?url=https%3A%2F%2Fsuper-token-launch-pad-base.vercel.app%2Fapi%2Faction%2FgetMetadata`
+        const castActionInstallationPageUrl = new URL(`/frames/castActionInstallationPage`, FRAMES_URL).href;
         const ogImageUrl = new URL(`/og/SuperTokenInput`, FRAMES_URL).href;
         const SuperTokenInputResp = `<!DOCTYPE html><html><head>
-            <title>Input Token Name</title>
-            <meta property="fc:frame" content="vNext" />
-            <meta property="of:accepts:xmtp" content="2024-02-01" /> 
-            <meta property="fc:frame:image" content="${ogImageUrl}"/>
-            <meta property="fc:frame:input:text" content="> Token Address"/>
-            <meta property="fc:frame:button:1" content="Create Cast Action ➡️" />
-            <meta property="fc:frame:button:1:action" content="link"/>
-            <meta property="fc:frame:button:1:target" content="${actionUrl}"/>
-            <meta property="fc:frame:post_url" content="${FRAMES_URL}/"/>
-            </head></html>`
+                    <title>Input Token Address</title>
+                    <meta property="fc:frame" content="vNext" />
+                    <meta property="of:accepts:xmtp" content="2024-02-01" /> 
+                    <meta property="fc:frame:image" content="${ogImageUrl}"/>
+                    <meta property="fc:frame:input:text" content="> Token Address"/>
+                    <meta property="fc:frame:button:1" content="Next ➡️" />
+                    <meta property="fc:frame:button:1:action" content="post"/>
+                    <meta property="fc:frame:post_url" content="${castActionInstallationPageUrl}/"/>
+                    </head></html>`
         return new NextResponse(SuperTokenInputResp)
     }
 
---
+++ app/frames/castActionInstallationPage/route.ts
@@ -0,0 +1,38 @@
+import { FrameRequest } from '@coinbase/onchainkit/frame';
+import { NextRequest, NextResponse } from 'next/server';
+  
+const FRAMES_URL = process.env.FRAMES_URL || "https://super-token-launch-pad-base.vercel.app";
+
+async function getResponse(req: NextRequest): Promise<NextResponse> { 
+    const body: FrameRequest = await req.json();
+    const inputText = body.untrustedData.inputText;
+   
+    const actionUrl = `https://warpcast.com/~/add-cast-action?url=https%3A%2F%2Fsuper-token-launch-pad-base.vercel.app%2Fapi%2Faction%2FgetMetadata%3FinputText%3D${inputText}`;
+    const ogImageUrl = new URL(`/og/SuperTokenInput`, FRAMES_URL).href; 
+    
+    
+                       
+        
+        
+        const SuperTokenInputResp = `<!DOCTYPE html><html><head>
+            <title>Input Token Name</title>
+            <meta property="fc:frame" content="vNext" />
+            <meta property="of:accepts:xmtp" content="2024-02-01" /> 
+            <meta property="fc:frame:image" content="${ogImageUrl}"/>
+            <meta property="fc:frame:button:1" content="Create Cast Action ➡️" />
+            <meta property="fc:frame:button:1:action" content="link"/>
+            <meta property="fc:frame:button:1:target" content="${actionUrl}"/>
+            <meta property="fc:frame:post_url" content="${FRAMES_URL}/"/>
+            </head></html>`
+        return new NextResponse(SuperTokenInputResp)
+   
+
+    return new NextResponse(``)
+}
+
+export async function POST(req: NextRequest): Promise<Response> {
+    return getResponse(req);
+}
+  
+export const dynamic = 'force-dynamic';
+  
