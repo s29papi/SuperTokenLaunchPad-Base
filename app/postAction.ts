2c0da9c8fe63c3d3eb817332142907fbdb70a00d
S29 <nots29vv@gmail.com>
2024-07-16 16:24:13 +0100

    commented inputTest

--- app/postAction.ts
+++ app/postAction.ts
@@ -14,20 +14,21 @@ type Message = {
     message: string;
 }
 
-export async function getResponsePOST(req: NextRequest, inputText: string): Promise<NextResponse> {
+export async function getResponsePOST(req: NextRequest): Promise<NextResponse> {
 
 
     const decodedUrl = decodeURIComponent(req.url);
-    // const match = decodedUrl.match(/inputText=([^&]*)/);
+    // const pattern2Match = '%3FinputText=' + 
+    const match = decodedUrl.match(/inputText/);
 
-    // if (!match) {
-    //     return new NextResponse('inputText not found in the provided URL', { status: 400 });
-    // }
+    if (!match) {
+        return new NextResponse('inputText not found in the provided URL', { status: 400 });
+    }
 
-    const extractedInputText = decodedUrl.substring(70, 72);
+    const extractedInputText = match.toString();
     
 
-    
+    const inputText = "dkdk"
     let actionFrame: ActionFrame = {
         type: "frame",
         frameUrl: `https://super-token-launch-pad-base.vercel.app/frames/castActionFirstPage?inputText=${inputText}&&reqUrl=${extractedInputText}`
