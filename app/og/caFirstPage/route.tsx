e11c0b55f1bd7b2ba73aa22417e44ebc92f9b1c7
S29 <nots29vv@gmail.com>
2024-07-27 14:28:37 +0100

    cast action chnages first page

--- app/og/caFirstPage/route.tsx
+++ app/og/caFirstPage/route.tsx
@@ -11,12 +11,12 @@ export async function GET(req: NextRequest) {
     const kanitFontData = await fetch(
         new URL('../../../public/Kanit-ExtraBold.ttf', import.meta.url),
     ).then((res) => res.arrayBuffer());
-    // const tokenName = await getTokenName(tokenAddress);
-    // const imgVal = tokenName.toUpperCase();
+    const tokenName = await getTokenName(tokenAddress);
+    const imgVal = tokenName.toUpperCase();
     return new ImageResponse(
         (
             <div tw='flex w-full h-full justify-center items-center text-6xl text-white bg-blue-500' style={{fontFamily: 'Kanit ExtraBold'}}>
-                {/* {imgVal} */} bhj {tokenAddress}
+                {imgVal}
             </div>
         ),
         {
