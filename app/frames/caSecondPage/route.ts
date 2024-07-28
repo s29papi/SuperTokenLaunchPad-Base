81e9ec6c12b5d1d1101fb136e59246ba5bd2a583
S29 <nots29vv@gmail.com>
2024-07-28 18:14:55 +0100

    cast action chnages first page

--- app/frames/caSecondPage/route.ts
+++ app/frames/caSecondPage/route.ts
@@ -34,7 +34,7 @@ export async function POST(req: NextRequest) {
     }
     if (idx == 3) {        
         IMG_URL = `https://super-token-launch-pad-base.vercel.app/og/caSecondPage?idx=3`;
-        BTN = `Stream ${castAddressData} ➡️`
+        BTN = `Stream ${castAddressData?.verifiedAddresses[0]} ➡️`
         TX_URL = `https://super-token-launch-pad-base.vercel.app/tx/stream?st=${st}&&t=${castAddressData}`;
         POST_URL = `https://super-token-launch-pad-base.vercel.app/frames/castActionFirstPage?st=${st}&&t=${t}`;
     }
