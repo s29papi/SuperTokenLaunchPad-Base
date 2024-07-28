0dd6e8c022169d03d0e6078d8798bb1adbd3fa57
S29 <nots29vv@gmail.com>
2024-07-28 19:35:11 +0100

    cast action chnages first page

--- app/tx/stream/route.ts
+++ app/tx/stream/route.ts
@@ -21,9 +21,9 @@ async function getResponse(req: NextRequest): Promise<NextResponse | Response> {
       }
 
     let data = encodeFunctionData({
-        abi: parseAbi(['function downgrade(uint256) external']),
+        abi: parseAbi(['function downgrade(ISuperToken,address,int96) external']),
         functionName: 'downgrade',
-        args: [parseUnits(amount, 18)]
+        args: [`0x${st?.substring(2)}`, `0x47dEAF612F0769d99aDB653bA2d22bba79F26C42`, parseUnits(amount, 0)]
     })
 
     const txData: FrameTransactionResponse = {
@@ -54,15 +54,4 @@ export const dynamic = 'force-dynamic';
 //         functionName: 'setFlowrate',
 //         args: [`0x${st?.substring(2)}`, `0x47dEAF612F0769d99aDB653bA2d22bba79F26C42`, parseUnits(amount, 0)]
 //     })
-
-//     const txData: FrameTransactionResponse = {
-//         chainId: `eip155:${base.id}`,
-//         method: 'eth_sendTransaction',   
-//         params: {
-//             abi: [],
-//             data,
-//             to: `0xcfA132E353cB4E398080B9700609bb008eceB125`,
-//             value: parseEther('0.00000').toString(),
-//         },
-//     };
 
