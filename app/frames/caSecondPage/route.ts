0dc04afc664c5af4584823c9a1cb254fde20eb8e
S29 <nots29vv@gmail.com>
2024-08-12 13:08:37 +0100

    added daily, monthly and yearly streams

--- app/frames/caSecondPage/route.ts
+++ app/frames/caSecondPage/route.ts
@@ -35,9 +35,56 @@ export async function POST(req: NextRequest) {
     }
     if (idx == 3) {        
         IMG_URL = `https://super-token-launch-pad-base.vercel.app/og/caSecondPage?idx=3`;
-        BTN = `Stream ➡️`
+        BTN = `Stream `
         TX_URL = `https://super-token-launch-pad-base.vercel.app/tx/stream?st=${st}&&t=${castAddressData?.verifiedAddresses[0]}`;
         POST_URL = `https://super-token-launch-pad-base.vercel.app/frames/castActionFirstPage?st=${st}&&t=${t}`;
+
+        return new NextResponse(
+            `<!DOCTYPE html><html><head>     
+            <title>View on Drakula</title>
+            <meta property="fc:frame" content="vNext" />
+            <meta property="of:accepts:xmtp" content="2024-02-01" /> 
+            <meta property="og:image" content="${IMG_URL}"/>
+            <meta property="fc:frame:image" content="${IMG_URL}"/>
+            <meta property="fc:frame:input:text" content="Enter Amount ..."/>
+            <meta property="fc:frame:button:1" content="${BTN} Daily ➡️" />
+            <meta property="fc:frame:button:1:action" content="tx"/>
+            <meta
+                    property="fc:frame:button:1:target"
+                    content="${TX_URL}"
+            />
+            <meta
+                property="fc:frame:button:1:post_url"
+                content="${POST_URL}" 
+            />
+            <meta property="fc:frame:button:2" content="${BTN} Monthly ➡️" />
+            <meta property="fc:frame:button:2:action" content="tx"/>
+            <meta
+                    property="fc:frame:button:2:target"
+                    content="${TX_URL}"
+            />
+            <meta
+                property="fc:frame:button:2:post_url"
+                content="${POST_URL}" 
+            />
+            <meta property="fc:frame:button:3" content="${BTN} Yearly ➡️" />
+            <meta property="fc:frame:button:3:action" content="tx"/>
+            <meta
+                    property="fc:frame:button:3:target"
+                    content="${TX_URL}"
+            />
+            <meta
+                property="fc:frame:button:3:post_url"
+                content="${POST_URL}" 
+            />
+            </head></html>`,
+            {
+                status: 200,
+                headers: {
+                "Content-Type": "text/html",
+                },
+            },
+        );
     }
     
     return new NextResponse(
