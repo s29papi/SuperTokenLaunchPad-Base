bbfb24ed57592d453ac5cba6de9b8330242874bd
S29 <nots29vv@gmail.com>
2024-08-26 17:51:01 +0100

    updated first ui

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -1,28 +1,99 @@
-import { ImageResponse } from 'next/og';
-import { NextRequest } from 'next/server';
+/* eslint-disable @next/next/no-img-element  */
+// @ts-nocheck
+import { ImageResponse } from 'next/og'
 
 export const runtime = 'edge';
 
-
-
-export async function GET(req: NextRequest) {
-    const kanitFontData = await fetch(
-        new URL('../../public/Kanit-ExtraBold.ttf', import.meta.url),
-    ).then((res) => res.arrayBuffer());
+export async function GET(req: Request) {
     return new ImageResponse(
-        (
-            <div tw='flex w-full h-full justify-center items-center text-6xl text-white bg-blue-500' style={{fontFamily: 'Kanit ExtraBold'}}>
-                Based Super Token Launch Pad 🪙 
-            </div>
-        ),
-        {
-            fonts: [
-                {
-                    name: 'Kanit ExtraBold',
-                    data: kanitFontData,
-                    style: 'normal'
-                },
-            ]
-        }
+        ( 
+            <div
+                style={{
+                    alignItems: 'center',
+                    background: 'black',
+                    backgroundSize: '100% 100%',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    flexWrap: 'nowrap',
+                    height: '100%',
+                    justifyContent: 'center',
+                    textAlign: 'center',
+                    width: '100%',
+                }}
+            >
+                            {/* 
+                                Function: Flex box outside inner rounded box edge.
+                            */}
+                <div 
+                    tw='
+                        flex 
+                        justify-center
+                        '
+                >
+                             {/* 
+                                Function: inner rounded box edge.
+                             */}
+                    <div 
+                        tw='
+                            flex 
+                            rounded-[99px] 
+                            font-extrabold 
+                            top-59        
+                            p-12 
+                            pr-72 
+                            pb-[466px]  
+                            bg-[#1B1B1B] 
+                            bg-opacity-80 
+                            shadow-lg 
+                            text-white 
+                            tracking-wide  
+                            w-full 
+                            max-w-[1300px]
+                            '
+                    >
+                             {/* 
+                                Function: Wrapper around both texts, set the flex direction and distance from the top.
+                            */}
+                        <div 
+                            tw='
+                                flex 
+                                flex-col 
+                                top-39
+                                '
+                        >
+                            {/* 
+                                TEXT: LAUNCHIT
+                                Function: Handles text font i.e. styling, sizing.
+                            */}
+                            <div 
+                                tw='
+                                    flex
+                                '
+                                    style={{
+                                        fontSize: 106,
+                                        fontWeight: 'bolder'
+                                    }}
+                            >
+                                {"TICKERSWAP"}
+                            </div>
+                            {/* 
+                                TEXT: Launch, Wrap, Un-wrap, and Stream Super Tokens In-Frame On Base.
+                                Function: Handles text font i.e. styling, sizing.
+                            */}
+                            <div 
+                                tw='
+                                    flex
+                                '
+                                    style={{
+                                        fontSize: 26
+                                    }}
+                            >
+                                {"Launch, Wrap, Un-wrap, and Stream Super Tokens In-Frame On Base"}
+                            </div>
+                        </div>
+                    </div> 
+                </div>
+          </div>
+        )
     )
 }
